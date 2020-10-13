3d6c532ddc195a0b02ae95bd12e5922341f7f63b
Sergiu Bacanu <sergiu.bacanu@yahoo.com>
2020-10-13 16:48:54 +0300

    Minor adjustments. Updated README.

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Go demo repo
-Go lang demo repo containing an API and test.
+Go lang demo repo containing an API and tests.
 
 ## Requirements
 Check the GOPATH environment variable so that the build passes. Set it via `go env -w GOPATH={your path}`
 
 ## Testing
 The assertion package used comes from `stretchr/testify` https://github.com/stretchr/testify
-To update Testify to the latest version, use go get -u github.com/stretchr/testify.
+
+To update Testify to the latest version, use `go get -u github.com/stretchr/testify`.
+
+In order to run the tests, first local server needs to be started(this runs on port 9090 by default): `go run main`.
+Running the actual tests can be done by executing the following command: `go test` from the root of the project.
--- main_test.go
+++ main_test.go
@@ -13,15 +13,14 @@ func TestGetBooks(t *testing.T) {
 	booksJSON := `[{"id":"1","isbn":"111","title":"Book One","author":{"firstname":"John","lastname":"Doe"}},{"id":"2","isbn":"222","title":"Book Two","author":{"firstname":"Steve","lastname":"Smith"}}]`
 	response, err := http.Get("http://localhost:9090/api/books")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	defer response.Body.Close()
 
 	body, _ := ioutil.ReadAll(response.Body)
 
 	assert := assert.New(t)
-	assert.Nil(err)
-	assert.Equal(200, response.StatusCode, "Response code should be 200 (OK)")
+	assert.Equal(http.StatusOK, response.StatusCode, "Response code should be 200 (OK)")
 	assert.Equal(booksJSON, strings.ReplaceAll(string(body), "\n", ""), "The response body should be equal to booksJSON")
 }
 
@@ -29,14 +28,13 @@ func TestGetBook(t *testing.T) {
 	bookJSON := `{"id":"2","isbn":"222","title":"Book Two","author":{"firstname":"Steve","lastname":"Smith"}}`
 	response, err := http.Get("http://localhost:9090/api/books/2")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	defer response.Body.Close()
 
 	body, _ := ioutil.ReadAll(response.Body)
 
 	assert := assert.New(t)
-	assert.Nil(err)
-	assert.Equal(200, response.StatusCode, "Response code should be 200 (OK)")
+	assert.Equal(http.StatusOK, response.StatusCode, "Response code should be 200 (OK)")
 	assert.Equal(bookJSON, strings.ReplaceAll(string(body), "\n", ""), "The response body should be equal to booksJSON")
 }
